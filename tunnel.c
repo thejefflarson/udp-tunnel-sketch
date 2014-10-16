61530b3a3a15da0276cf4a4ffec7da71750100bd
Jeff Larson <thejefflarson@gmail.com>
2014-10-16 16:44:43 -0400

    sketched out recv and loop

--- tunnel.c
+++ tunnel.c
@@ -1,5 +1,3 @@
-
-
 const uint8_t HELLO = (1 << 0);
 const uint8_t HI    = (1 << 1);
 const uint8_t ACK   = (1 << 2);
@@ -7,8 +5,17 @@ const uint8_t DATA  = (1 << 3);
 uint16_t seq = 0;
 int sockfd;
 
+typedef struct rudp_queue {
+  struct sockaddr_storage addr;
+  uint8_t *data;
+  size_t length;
+  rudp_queue *next;
+} rudp_queue_t;
+
+rudp_queue_t *queue = NULL;
+
 void
-loop(){
+loop(rudp_queue_t *queue){
   fd_set readfd, writefd;
   FD_ZERO(&readfd);
   FD_ZERO(&writefd);
@@ -20,18 +27,36 @@ loop(){
     tv.tv_usec = 0;
     select(1, &readfd, &writefd, NULL, &tv);
     if(IS_SET(sockfd, &readfd)) {
-
+      struct sockaddr_storage addr;
+      char data[65536];
+      sendto(sockfd, data, 65536, addr, sizeof(addr));
+      rudp_recv(addr, data, length);
     }
     if(IS_SET(sockfd, &writefd)) {
       // send waiting packets
-
+      if(rudp_queue_t *queue) {
+        rudp_queue_t *head = queue;
+        sendto(sockfd, head->data, head->length, head->addr, sizeof(head->addr));
+        queue = head->next;
+        free(head);
+      }
     }
   }
 }
 
 void
 rudp_send(struct sockaddr_storage addr, uint8_t *data, int length) {
-  sendto(sockfd, data, length, addr, sizeof(addr));
+  rudp_queue_t *tail = queue;
+  if(tail == NULL) {
+    tail = calloc(1, sizeof(rudp_queue_t));
+  } else {
+    while(tail->next != NULL) tail = tail->next;
+    tail->next = calloc(1, sizeof(rudp_queue_t));
+    tail = tail->next;
+  }
+  tail->addr = addr;
+  tail->data = data;
+  tail->length = length;
 }
 
 void
