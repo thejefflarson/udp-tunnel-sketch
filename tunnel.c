8ef99c0c82045c9bbc73313a53ed0c6ad1bf9a22
Jeff Larson <thejefflarson@gmail.com>
2014-10-31 00:44:42 -0400

    no errors

--- tunnel.c
+++ tunnel.c
@@ -1,3 +1,10 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <sys/select.h>
+#include <sys/socket.h>
+#include <time.h>
+
 const uint8_t HELLO = (1 << 0);
 const uint8_t HI    = (1 << 1);
 const uint8_t ACK   = (1 << 2);
@@ -8,10 +15,15 @@ typedef struct rudp_queue {
   struct sockaddr_storage addr;
   uint8_t *data;
   size_t length;
-  rudp_queue *next;
+  struct rudp_queue *next;
 } rudp_queue_t;
 
 rudp_queue_t *queue = NULL;
+
+void
+rudp_recv(struct sockaddr_storage addr, uint8_t *data, int length);
+void
+rudp_send(struct sockaddr_storage addr, uint8_t *data, int length);
 
 void
 loop(rudp_queue_t *queue){
@@ -21,21 +33,21 @@ loop(rudp_queue_t *queue){
   FD_SET(sockfd, &readfd);
   FD_SET(sockfd, &writefd);
   while(1) {
-    timeval tv;
+    struct timeval tv;
     tv.tv_sec  = 0;
     tv.tv_usec = 0;
     select(1, &readfd, &writefd, NULL, &tv);
-    if(IS_SET(sockfd, &readfd)) {
+    if(FD_ISSET(sockfd, &readfd)) {
       struct sockaddr_storage addr;
       uint8_t data[65536];
-      sendto(sockfd, data, 65536, addr, sizeof(addr));
+      ssize_t length = sendto(sockfd, data, 65536, 0, (struct sockaddr *) &addr, sizeof(addr));
       rudp_recv(addr, data, length);
     }
-    if(IS_SET(sockfd, &writefd)) {
-      if(rudp_queue_t *queue) {
+    if(FD_ISSET(sockfd, &writefd)) {
+      if(queue != NULL) {
         rudp_queue_t *head = queue;
         if(head != NULL) {
-          sendto(sockfd, head->data, head->length, head->addr, sizeof(head->addr));
+          sendto(sockfd, head->data, head->length, 0, (struct sockaddr *) &head->addr, sizeof(head->addr));
           queue = head->next;
           free(head->data);
           free(head);
@@ -63,16 +75,20 @@ rudp_send(struct sockaddr_storage addr, uint8_t *data, int length) {
 
 void
 rudp_recv(struct sockaddr_storage addr, uint8_t *data, int length) {
-  proto_t prot = data[0];
-  switch(prot) {
+  uint8_t flags = *data;
+  uint8_t ack   = flags & ACK;
+  flags        &= ~ACK; // clear the ACK bit
+  // if statements here
+  switch(flags) {
     case HELLO:
-      send(addr);
+      rudp_send(addr, data, length);
+      break;
     case HI:
-      send(addr);
-    case ACK:
-      send(addr);
+      rudp_send(addr, data, length);
+      break;
     case DATA:
-      send(addr);
+      rudp_send(addr, data, length);
+      break;
     default:
       puts("error!");
   }
