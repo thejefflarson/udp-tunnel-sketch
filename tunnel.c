bfe9b6829c58de95d2fe931c800503a4fe814c17
Jeff Larson <thejefflarson@gmail.com>
2014-11-07 02:34:17 -0500

    waypoint

--- tunnel.c
+++ tunnel.c
@@ -1,3 +1,4 @@
+#include <arpa/inet.h>
 #include <memory.h>
 #include <stdlib.h>
 #include <stdio.h>
@@ -5,10 +6,14 @@
 #include <sys/select.h>
 #include <sys/socket.h>
 #include <time.h>
+#include "tweetnacl.h"
 
-const uint8_t HELLO = (1 << 0);
-const uint8_t HI    = (1 << 1);
-const uint8_t DATA  = (1 << 2);
+const uint8_t HELLO = (1 << 0); // syn pubkey
+const uint8_t HI    = (1 << 1); // ack pubkey
+const uint8_t BYE   = (1 << 2); // close
+const uint8_t DATA  = (1 << 3); // encrypted data
+
+
 int sockfd;
 uint16_t seq = 0;
 
@@ -20,6 +25,8 @@ typedef struct rudp_queue {
 } rudp_queue_t;
 
 rudp_queue_t *queue = NULL;
+uint8_t their_key[crypto_secretbox_KEYBYTES] = {0};
+uint8_t our_key[crypto_secretbox_KEYBYTES] = {0};
 
 int
 rudp_recv(struct sockaddr_storage addr, uint8_t *data, int length);
@@ -60,7 +67,7 @@ loop(rudp_queue_t *queue){
   }
 }
 
-void
+int
 rudp_send(struct sockaddr_storage addr, uint8_t *data, int length) {
   rudp_queue_t *tail = queue;
   if(tail == NULL) {
@@ -74,34 +81,45 @@ rudp_send(struct sockaddr_storage addr, uint8_t *data, int length) {
   tail->addr = addr;
   tail->data = data;
   tail->length = length;
+  return 0;
 }
 
 int
 rudp_recv(struct sockaddr_storage addr, uint8_t *data, int length) {
-  size_t hlen = sizeof(uint8_t) + sizeof(uint16_t) + sizeof(uint32_t);
-  if(length < hlen) return -1;
+  size_t len;
   uint8_t flags = *data;
-  uint16_t *ack = (uint16_t *)data + 1;
   switch(flags) {
-    case HELLO:;
-      size_t length = 1; // protocol
-      length += sizeof(uint32_t); // ack number
-      length += sizeof(uint32_t); // seq number
-      length += sizeof(uint8_t) * 32; // public key
-      uint8_t *reply = calloc(1 * 4 * 32, sizeof(uint8_t));
+    case HELLO:
+      len = sizeof(their_key) + sizeof(HELLO);
+      if(length < len) return -1;
+      memcpy(their_key, data + 1, sizeof(their_key));
+      len = sizeof(their_key) + sizeof(HI);
+      len += crypto_secretbox_NONCEBYTES + crypto_secretbox_BOXZEROBYTES;
+      uint8_t *reply = calloc(len, sizeof(uint8_t));
       reply[0] = HI;
-      memcpy(reply + 1, ack, sizeof(*ack));
+      reply += sizeof(HI);
+      uint16_t nack = htons(0);
+      memcpy(reply, &nack, sizeof(nack));
+      reply += sizeof(nack);
       seq++;
-      memcpy(reply + 1 + sizeof(ack), &seq, sizeof(seq));
-      rudp_send(addr, reply, length);
+      uint16_t nseq = htons(seq);
+      memcpy(reply + 1 + sizeof(seq), &nseq, sizeof(nseq));
+      reply += sizeof(seq);
+      // encrypt our_key;
+      rudp_send(addr, reply, len);
       break;
-    case HI:
-      rudp_send(addr, data, length);
+    case HI:;
+      size_t len = crypto_secretbox_KEYBYTES;
+      if(length < len) return -1;
+      // decrypt our_key and ensure it is the same
       break;
-    case DATA:
-      rudp_send(addr, data, length);
+    case DATA:;
+      uint16_t ack = ntohs(*(uint16_t *)(data + 1));
+      rudp_send(addr, data, 0);
       break;
     default:
       puts("error!");
+      return -1;
   }
+  return 0;
 }
